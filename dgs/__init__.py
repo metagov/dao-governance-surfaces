ad2aa42bc9ef1a702a96baa31c672c8b5e42ed44
notchia <lucia.korpas@gmail.com>
2022-12-19 23:17:31 -0600

    update and test github module

--- dgs/__init__.py
+++ dgs/__init__.py
@@ -8,13 +8,19 @@ logging.basicConfig(level=logging.DEBUG)
 sys.path.append("solidity-parser")
 
 TMPDIR = 'tmp'
+CONFIGDIR = 'config'
+SAVEDIR = TMPDIR
 
 if not os.path.isdir(TMPDIR):
     os.mkdir(TMPDIR)
 
-import json
+REPO_CONFIG_FILE = os.path.join(CONFIGDIR, 'repodicts.csv')
+if not os.path.isfile(REPO_CONFIG_FILE):
+    if not os.path.isdir(CONFIGDIR):
+        os.mkdir(CONFIGDIR)
+    open(REPO_CONFIG_FILE, 'a').close()
 
-KEYWORD_CONFIG_FILE = 'config/keywords.json'
+KEYWORD_CONFIG_FILE = os.path.join(CONFIGDIR, 'keywords.json')
 assert os.path.isfile(KEYWORD_CONFIG_FILE ), "Provide a keyword file"
 with open(KEYWORD_CONFIG_FILE, 'r') as f:
     CODING = json.load(f)['categories']
